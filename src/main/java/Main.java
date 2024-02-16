31835cc61af5b800b495fd469831d9c6780934ae
david <dsuon04@gmail.com>
2024-02-16 14:30:24 -0800

    executor services

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,46 +1,66 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+  public static void main(String[] args) {
+      // You can use print statements as follows for debugging, they'll be visible when running tests.
+      System.out.println("Logs from your program will appear here!");
 
-    //  Uncomment this block to pass the first stage
-       ServerSocket serverSocket = null;
-       Socket clientSocket = null;
-       int port = 6379;
-       try {
-         serverSocket = new ServerSocket(port);
-         serverSocket.setReuseAddress(true);
-         // Wait for connection from client.
-         clientSocket = serverSocket.accept();
+      ExecutorService executorService = Executors.newCachedThreadPool();
+      //  Uncomment this block to pass the first stage
+      ServerSocket serverSocket = null;
 
-         InputStream inputStream = clientSocket.getInputStream();
-         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+      final int port = 6379;
+//      final Socket clientSocket;
+      try {
+          serverSocket = new ServerSocket(port);
+          serverSocket.setReuseAddress(true);
 
-         OutputStream outputStream = clientSocket.getOutputStream();
+          while (true) {
+              try {
+                 final Socket clientSocket = serverSocket.accept();
+                  executorService.execute(() -> handle(clientSocket));
+              } catch (Exception e) {
+                  System.out.println("IOException: " + e.getMessage());
+              }
+          }
+//         outputStream.close();
+//         reader.close();
+      } catch (IOException e) {
+//          executorService.close();
+          System.out.println("IOException: " + e.getMessage());
+      }
+//         try {
+//           if (clientSocket != null) {
+//             clientSocket.close();
+//           }
+//         } catch (IOException e) {
+//           System.out.println("IOException: " + e.getMessage());
+//         }
+//       }
+      }
+
+
+      public static void handle (Socket clientSocket){
+          try {
+              InputStream inputStream = clientSocket.getInputStream();
+              BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+              OutputStream outputStream = clientSocket.getOutputStream();
 //         OutputStreamWriter writer = new OutputStreamWriter(outputStream);
-         String command;
-         while ( (command = reader.readLine()) != null){
-             if (command.equals("ping")){
-                 outputStream.write("+PONG\r\n".getBytes());
-             }
-         }
-//         outputStream.write("+PONG\r\n".getBytes());
-         outputStream.close();
-         reader.close();
-       } catch (IOException e) {
-         System.out.println("IOException: " + e.getMessage());
-       } finally {
-         try {
-           if (clientSocket != null) {
-             clientSocket.close();
-           }
-         } catch (IOException e) {
-           System.out.println("IOException: " + e.getMessage());
-         }
-       }
+              String command;
+              while ((command = reader.readLine()) != null) {
+                  if (command.equals("ping")) {
+                      outputStream.write("+PONG\r\n" .getBytes());
+                      outputStream.flush();
+                  }
+              }
+          } catch (Exception e) {
+              System.out.println("Issue occurred in handle " + e.getMessage());
+          }
+
+      }
   }
-}
