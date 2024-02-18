45b3bf9b07dd7fbcccb721a95329e7f0c73f34ff
david <dsuon04@gmail.com>
2024-02-17 16:02:23 -0800

    refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,13 +19,10 @@ public class Main {
 
       // You can use print statements as follows for debugging, they'll be visible when running tests.
       System.out.println("Logs from your program will appear here!");
-
       ExecutorService executorService = Executors.newCachedThreadPool();
-//      ServerSocket serverSocket = null;
 
       final int port = 6379;
       try(ServerSocket serverSocket = new ServerSocket(port)) {
-//          serverSocket = new ServerSocket(port);
           serverSocket.setReuseAddress(true);
 
           while (!serverSocket.isClosed()) {
@@ -53,7 +50,6 @@ public class Main {
 
               String command;
               while ((command = reader.readLine()) != null) {
-                  System.out.println("COMMAND is: " + command);
                   if (command.startsWith("*")){
                       int numOfItems = Integer.parseInt(command.substring(1));
                       ArrayList<String> inputs = new ArrayList<>(numOfItems * 2);
@@ -63,14 +59,13 @@ public class Main {
                       }
                       String cmd = inputs.get(1);
                       switch (cmd.toLowerCase()) {
-                          case Constants.CMD_PING:
+                          case Constants.CMD_PING ->
                               outputStream.write( new Ping().print(inputs, cache));
-                              outputStream.flush();
-                          case Constants.CMD_ECHO:
+                          case Constants.CMD_ECHO ->
                               outputStream.write( new Echo().print(inputs,cache));
-                          case Constants.CMD_SET:
+                          case Constants.CMD_SET ->
                               outputStream.write(new Set().print(inputs, cache));
-                          case Constants.CMD_GET:
+                          case Constants.CMD_GET ->
                               outputStream.write(new Get().print(inputs, cache));
                       }
                   }
