5ddb22980b73af658279627879ce7b9b48c3da82
david <dsuon04@gmail.com>
2024-02-19 23:58:35 -0800

    why not setting

--- src/main/java/command/Get.java
+++ src/main/java/command/Get.java
@@ -8,12 +8,12 @@ import java.util.Map;
 public class Get implements Command{
     @Override
     public byte[] print(List<String> inputs, Map<String, String> cache) {
-        inputs.stream().forEach(System.out::println);
+//        inputs.stream().forEach(System.out::println);
         String key = inputs.get(3);
 
         String value = cache.get(key);
-//"$-1\r\n"
-        String result = value == null ? "123\r\n" : "+" + value + "\r\n";
+
+        String result = value == null ? "$-1\r\n" : "+" + value + "\r\n";
 
         System.out.println("ret: " + result);
 
@@ -23,6 +23,14 @@ public class Get implements Command{
     public byte[] printWithLoggingType(Redis redis, List<String> inputs, Map<String, String> cache){
         System.out.println(redis.getClass() + ", role: " + redis.getRole());
 
-        return print(inputs,cache);
+        String key = inputs.get(3);
+
+        String value = cache.get(key);
+
+        String result = value == null ? "123\r\n" : "+" + value + "\r\n";
+
+        System.out.println("ret: " + result);
+
+        return result.getBytes();
     }
 }
